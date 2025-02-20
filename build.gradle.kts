223059cc5cbabb96d42eb0835e2ed64b36a21a5f
Yoan Dusoleil <y.dusoleil@outlook.com>
2025-02-20 10:17:35 +0100

    feat: add Kafka setup with Zookeeper and topic creation in Main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.kafka:kafka-clients:3.5.1")
 }
 
 tasks.test {
